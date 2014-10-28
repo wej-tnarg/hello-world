eb58b2d44cf27dfcc4d35bfa921cadd08547247b
grant jew <grant.jew@virginpulse.com>
2014-10-28 10:18:51 -0400

    code checkin

---
+++ test/test.js
@@ -0,0 +1,9 @@
+// test.js
+var request = require('supertest');
+var app = require('../app.js');
+
+describe('GET /', function() {
+	it('respond with hello world', function(done) {
+		request(app).get('/').expect('hello world', done);
+	});
+});
